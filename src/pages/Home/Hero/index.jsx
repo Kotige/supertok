91b43b7253cf8cafc6663b10d647718772cc86d6
Vitor <vitor_fernandes_pie@yahoo.com.br>
2026-02-13 18:20:29 -0300

    style: add basic style for hero section

--- src/pages/Home/Hero/index.jsx
+++ src/pages/Home/Hero/index.jsx
@@ -1,7 +1,19 @@
+import "./styles.scss";
+import models from "../../../assets/images/hero/models.png";
+import logo from "../../../assets/images/hlogo.png";
+
 export default function Hero() {
     return (
-        <section id="home" className="mt-5 h-[100vh] flex justify-center items-center">
-            <h1 className="text-5xl text-off text-center">Hero</h1>
+        <section id="home" className="mt-5 h-[90vh] flex justify-center items-center">
+            <div className="bg-hero"/>
+            <div className="flex gap-15 mx-12 hero-content">
+                <div className="flex justify-center items-center">
+                    <img src={logo} alt="logo" className="w-[100%] h-[30%]"/>
+                </div>
+                <div className="flex items-end justify-end">
+                    <img src={models} alt="ciclistas modelos"  className="w-[60%]"/>
+                </div>
+            </div>
         </section>
     )
 }
