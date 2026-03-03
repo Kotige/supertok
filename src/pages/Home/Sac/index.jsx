ca5e89f570f5d7c74e496933bb336d269a48bce0
Vitor <vitor_fernandes_pie@yahoo.com.br>
2026-03-03 15:49:00 -0300

    feat(sac): add sac section

--- src/pages/Home/Sac/index.jsx
+++ src/pages/Home/Sac/index.jsx
@@ -1,7 +1,73 @@
+import { useState } from "react";
+import { ChevronDown } from "lucide-react";
+
 export default function Sac() {
+
+    const [openIndex, setOpenIndex] = useState(null);
+
+    const faqs = [
+        {
+            question: "Onde posso comprar os produtos Super Tok?",
+            answer: "Não. Os produtos Super Tok são desenvolvidos para oferecer alta eficiência na limpeza sem agredir as superfícies quando utilizamos corretamente. Recomendamos sempre seguir o modo de uso indicado no rótulo e, em caso de dúvida ou superfícieis mais sensíveis, realizar um teste prévio em uma pequena área antes da aplicação do produto."
+        },
+        {
+            question: "Os produtos são indicados para uso profissional?",
+            answer: "Não. Os produtos Super Tok são desenvolvidos para oferecer alta eficiência na limpeza sem agredir as superfícies quando utilizamos corretamente. Recomendamos sempre seguir o modo de uso indicado no rótulo e, em caso de dúvida ou superfícieis mais sensíveis, realizar um teste prévio em uma pequena área antes da aplicação do produto."
+        },
+        {
+            question: "Os produtos podem danificar superfícies?",
+            answer: "Não. Os produtos Super Tok são desenvolvidos para oferecer alta eficiência na limpeza sem agredir as superfícies quando utilizamos corretamente. Recomendamos sempre seguir o modo de uso indicado no rótulo e, em caso de dúvida ou superfícieis mais sensíveis, realizar um teste prévio em uma pequena área antes da aplicação do produto."
+        },
+        {
+            question: "Como armazenar corretamente?",
+            answer: "Não. Os produtos Super Tok são desenvolvidos para oferecer alta eficiência na limpeza sem agredir as superfícies quando utilizamos corretamente. Recomendamos sempre seguir o modo de uso indicado no rótulo e, em caso de dúvida ou superfícieis mais sensíveis, realizar um teste prévio em uma pequena área antes da aplicação do produto."
+        }
+    ];
+
+    const handleToggle = (index) => {
+        setOpenIndex(openIndex === index ? null : index);
+    };
+
     return (
-        <section id="sac" className="h-[100vh]">
-            <h2 className="text-3xl text-off">SAC</h2>
+        <section id="sac" className="bg-black pt-8">
+            <h2 className="text-3xl text-off text-center font-bold mb-6">
+                <span className="text-gold">Dúvidas</span> frequentes
+            </h2>
+
+            <div className="max-w-3xl mx-6 space-y-4">
+                {faqs.map((faq, index) =>(
+                    <div key={index}
+                        className=" overflow-hidden"
+                    >
+                        <button
+                            onClick={() => handleToggle(index)}
+                            className="w-full flex justify-between items-center p-5 text-left text-off hover:bg-off/5 transition"
+                        >
+                            <span className={`text-sm ${openIndex === index ? "font-bold" : ""}`}>
+                                {faq.question}
+                            </span>
+
+                            <ChevronDown 
+                                className={`transition-transform duration-300 ${openIndex === index ? "rotate-180" : ""}`}
+                            />
+                        </button>
+                        <div
+                            className={`grid transition-all duration-300 ease-in-out ${
+                                openIndex === index
+                                ? "grid-rows-[1fr] opacity-100"
+                                : "grid-rows-[0fr] opacity-0"
+                            }`}
+                        >
+                            <div className="overflow-hidden border border-off/30">
+                                <p className="p-5  text-off text-center text-sm">
+                                    {faq.answer}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
         </section>
     )
 }
