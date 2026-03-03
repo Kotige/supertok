f1517786e4f4b4e4f23d9c7277b866a5e6c12c34
Vitor <vitor_fernandes_pie@yahoo.com.br>
2026-03-03 19:14:04 -0300

    feat(footer): add footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,18 @@
+import modv from "../../assets/images/modv.png"
+
 export default function Footer() {
     return (
-        <footer>
-            <h3 className="text-lx text-center text-off">footer</h3>
+        <footer className="bg-black text-off">
+            <div className="flex flex-col items-center justify-center">
+                <a href="https://modvestudio.com.br/" target="_blank">
+                    <img 
+                        src={modv} 
+                        alt="modv estúdio"
+                        className="w-50 my-15"
+                    />
+                </a>
+                <p className="text-sm mb-15">© 2026 modv estúdio. Todos os direitos reservados.</p>
+            </div>
         </footer>
     )
 }
